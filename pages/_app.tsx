ac10e2ef6354538ed4c0340bf728e4099c200201
Augustin Briolon <augustin.briolon@gmail.com>
2025-12-15 16:28:01 +0100

    init project

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import Layout from '@/layout/default';
+import { AppProvider } from '@/providers/root';
+import '@/styles/main.scss';
+import '@/styles/tailwind.css';
+import { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <AppProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AppProvider>
+    </>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Hero from '@/features/shared/hero';
+
+export default function Page() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
